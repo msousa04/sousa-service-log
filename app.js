e899b518d79c69ef5d25e6984a9c6c78e33054fe
msousa04 <msousa04@yahoo.com>
2025-07-13 11:30:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log('Sousa Service Log loaded.');
---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sousa-log-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
